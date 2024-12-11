b04df56ff1bdf4dc65c5d8fd09028703eb275445
AOA19 <aakinyemi19@gmail.com>
2024-12-11 16:20:51 -0500

    Added more styling

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,7 @@
 // const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Gallery = require("../models/Gallery")
+const Journal = require("../models/Journal")
 
 
 module.exports = {
@@ -9,7 +10,9 @@ module.exports = {
       const posts = await Post.find({ user: req.user.id });
       // Fetch the latest 3 images from the gallery
       const latestImgs = await Gallery.find({user: req.user.id}).sort({createdAt: -1}).limit(3);
-      res.render("dashboard.ejs", { posts: posts, user: req.user, images: latestImgs });
+      // Fetch latest journal entry
+      const latestEntry = await Journal.findOne({user:req.user.id}).sort({createdAt: -1}).exec();
+      res.render("dashboard.ejs", { posts: posts, user: req.user, images: latestImgs, latestEntry });
     } catch (err) {
       console.log(err);
     }
